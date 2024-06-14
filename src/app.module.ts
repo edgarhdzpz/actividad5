690898f9d092a01fc1653db2d6935e5742d2246b
Edgar <herreraperezedgar98@gmail.com>
2024-06-14 13:33:53 -0600

    implemente crud municipios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MunicipiosModule } from './municipios/municipios.module';
 
 @Module({
-  imports: [],
+  imports: [MunicipiosModule],
   controllers: [AppController],
   providers: [AppService],
 })
