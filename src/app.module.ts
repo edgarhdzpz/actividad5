2288a9f68905889438d905faf4301a713affbfd1
JhonyDev09 <jhonatanolivar09@gmail.com>
2024-06-14 14:01:12 -0600

    Implementa funcionalidad crud de clientes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientesService } from './clientes/clientes.service';
+import { ClientesModule } from './clientes/clientes.module';
 
 @Module({
-  imports: [],
+  imports: [ClientesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ClientesService],
 })
 export class AppModule {}
